e7adf8ed117698e8dd09d3dcae74df5e8c21bc08
Tina Wang <tinary@users.noreply.github.com>
2020-03-23 15:16:45 -0400

    update descriptions

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Interactive creative coding
-I made projects using p5.js libraries.
+I made drawing canvas with p5.js libraries.
 
 `sketchSpaceship.js`
 Using simple math to create shapes and animations to make a floating spaceship.
 
 `sketchFlashingStars.js`
-Using simple math to create shapes and integrating mouse click to add that interactive drawing. 
+Using trigonometry to create star shapes and integrating mouse click function for interactive drawing. 
 
 # How to run the code
 If you have VS Code, simply open index.html with a live server. Otherwise, check out this URL to run a local server https://github.com/processing/p5.js/wiki/Local-server
--- sketchFlashingStars.js
+++ sketchFlashingStars.js
@@ -1,4 +1,4 @@
-//Spaceship flying forward
+//Draw stars on a canvas
 
 let img;
 
@@ -19,9 +19,10 @@ function draw() {
   //earth image
   image(img, windowWidth / 2, 220, img.width / 4, img.height / 4);
 
-  //press the mouse key and move around to draw stars
+  //Color of the stars
   fill(255 + sin(frameCount * 0.1) * 255, 204, 0);
 
+  //Press the mouse key and move around to draw stars
   if (mouseIsPressed) {
     translate(width * 0.4, height * 0.2);
     star(mouseX - 680, mouseY - 180, 3, 7, 5);
@@ -29,6 +30,24 @@ function draw() {
   else {
     noStroke();
   }
+}
+
+
+
+//Star shape
+function star(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
 
 
@@ -50,21 +69,3 @@ function draw() {
 //   //   star(mouseX - 680, mouseY - 180, 3, 7, 5);
 //   // }
 // }
-
-
-
-//star shape
-function star(x, y, radius1, radius2, npoints) {
-  let angle = TWO_PI / npoints;
-  let halfAngle = angle / 2.0;
-  beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
-    let sx = x + cos(a) * radius2;
-    let sy = y + sin(a) * radius2;
-    vertex(sx, sy);
-    sx = x + cos(a + halfAngle) * radius1;
-    sy = y + sin(a + halfAngle) * radius1;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
-}
